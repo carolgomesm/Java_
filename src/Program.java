31a7660b86be0f815fa6f4c985104ca24016f696
carolgomesm <carolgomesm@gmail.com>
2015-10-27 10:24:32 -0400

    Add constructors, getters and setters

--- src/Program.java
+++ src/Program.java
@@ -1,11 +1,21 @@
 
 public class Program {
 
-	private static String _name = "";
+	public static Person person; //reference to the person class
+	
 	public static void main(String[] args) {
 		System.out.println("Program starter");
-		_name = "Carol";
-		System.out.println(_name);
+		
+		person = new Person(27);
+		
+		person.setName("Carol");
+		
+		System.out.println(person.getName());
+		
+		person.runs();
+		
+		
+
 	}
 
 }
